94e0d56857ec6e7b844db97d34cb39ac0ba1ae67
ivan.andreski <ivan.andreski@gmail.com>
2023-07-03 08:27:13 +0200

    init2

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "pc-builder-mk",
+        short_name: "pc-builder-mk",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icons/512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        type: "module",
+      },
+    }),
+  ],
+});
